ff00a2a1ed2d03c2e699b7596d8fc9cf2ac1f8e5
kruthik693 <kruthik693@gmail.com>
2026-04-15 16:55:12 +0530

    In Process

--- src/main/java/com/xworkz/ecomerceApp/restController/Controller.java
+++ src/main/java/com/xworkz/ecomerceApp/restController/Controller.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+
 @RestController
 @RequestMapping("/")
-@org.springframework.stereotype.Controller
 public class Controller {
     @Autowired
     UserService userService;
